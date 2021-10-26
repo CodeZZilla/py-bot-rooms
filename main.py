830046fac454c93699971d3e0119b2676bc04d33
Dima <dimmaa9@gmail.com>
2021-10-26 17:33:38 +0300

    update v0.8

--- main.py
+++ main.py
@@ -19,11 +19,6 @@ logging.basicConfig(filename='file.log',
 
 TELEGRAM_TOKEN = ''
 tranzzo_token = ''
-with open('tokens') as file:
-    arr = file.readlines()
-    arr = [line.rstrip() for line in arr]
-    TELEGRAM_TOKEN = arr[0].split(':')[1]
-    tranzzo_token = arr[1].split(':')[1]
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
