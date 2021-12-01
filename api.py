3b2132e0bb9f4b8e7980357725c97ac1f951ce7a
Dima <dimmaa9@gmail.com>
2021-12-01 14:13:06 +0200

    update v3.10 (alpha)

--- api.py
+++ api.py
@@ -2,12 +2,23 @@ import requests
 
 link = 'http://localhost:8080/api'  #95.217.133.188
 
+admins_telegram_id = ['412306507', '263041096']
 
-admins_telegram_id = ['', '', '']
+
+def get_token():
+    req = requests.post(link + '/auth/signin', headers={'Content-Type': 'application/json'}, json={
+        'username': "rooms",
+        'password': "zsxadc1234"
+    })
+    output = req.json()
+    return output.token
 
 
 def create_user(message):
-    req = requests.post(link + '/user/add', headers={'Content-Type': 'application/json'}, json={
+    req = requests.post(link + '/user/add', headers={
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + get_token()
+    }, json={
         'nickname': message.from_user.username,
         'name': message.from_user.first_name,
         'idTelegram': message.from_user.id,
@@ -29,22 +40,28 @@ def create_user(message):
 
 
 def check_user(message):
-    req = requests.get(link + '/user/' + str(message.from_user.id))
+    req = requests.get(link + '/user/' + str(message.from_user.id), headers={
+        'Authorization': 'Bearer ' + get_token()
+    })
     if req.status_code == 404:
         return False
     return True
 
 
 def get_user(id_telegram):
-    req = requests.get(link + '/user/' + str(id_telegram))
+    req = requests.get(link + '/user/' + str(id_telegram), headers={
+        'Authorization': 'Bearer ' + get_token()
+    })
     return req.json()
 
 
 def update_field_for_user(id_telegram, value, str_field):
     user = get_user(id_telegram)
     user[str_field] = value
-    req = requests.put(link + '/user/updateById/' + str(user['id']), headers={'Content-Type': 'application/json'},
-                       json=user)
+    req = requests.put(link + '/user/updateById/' + str(user['id']), headers={
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + get_token()
+    }, json=user)
     return req.json()
 
 
@@ -59,7 +76,9 @@ def random_apartment(id_telegram):
         'subLocationName': user['region'],
         'metro': user['metroNames']
     }
-    req = requests.get(link + '/apartments/randomByParams', params=params)
+    req = requests.get(link + '/apartments/randomByParams', params=params, headers={
+        'Authorization': 'Bearer ' + get_token()
+    })
     return req.json()
 
 
@@ -67,17 +86,24 @@ def find_apartment(id_apartment):
     params = {
         'id': [int(id_apartment)]
     }
-    req = requests.get(link + '/apartments/find', params=params)
+    req = requests.get(link + '/apartments/find', params=params, headers={
+        'Authorization': 'Bearer ' + get_token()
+    })
     return req.json()
 
 
 def update_url(obj_id_apartment, language, new_url):
     req = requests.put(link + '/apartments/updateUrl/' + str(obj_id_apartment),
-                       headers={'Content-Type': 'application/json'},
+                       headers={
+                           'Content-Type': 'application/json',
+                           'Authorization': 'Bearer ' + get_token()
+                       },
                        json='{"url": ' + new_url + '}')
     return req.json()
 
 
 def get_users_messages():
-    req = requests.get(link + '/message/find')
+    req = requests.get(link + '/message/find', headers={
+        'Authorization': 'Bearer ' + get_token()
+    })
     return req.json()
